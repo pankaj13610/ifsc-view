0a53e0556556c12b5e92a17ff87e925882e9344c
Pankaj Kumar Singh <pankaj.ponder@gmail.com>
2021-03-16 17:12:47 +0530

    Show bank details in ui using stubbed IFSCResponse object

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-
+import { IFSCResponse } from './ifsc-response';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ifsc-view';
   code = '';
+  result: IFSCResponse = {
+    CENTRE: 'NA',
+    STATE: 'KARNATAKA',
+    DISTRICT: 'MANGALORE',
+    SWIFT: '',
+    MICR: 'NA',
+    BRANCH: 'Karnataka Bank IMPS',
+    CONTACT: '2228222',
+    NEFT: true,
+    CITY: 'DAKSHINA KANNADA',
+    RTGS: false,
+    UPI: true,
+    IMPS: true,
+    ADDRESS:
+      'REGD. & HEAD OFFICE, P.B.NO.599, MAHAVEER CIRCLE, KANKANADY, MANGALORE - 575002',
+    BANK: 'Karnataka Bank',
+    BANKCODE: 'KARB',
+    IFSC: 'KARB0000001',
+  };
+
   search() {
     console.log(`Seaching for ${this.code}`);
   }
---
+++ src/app/ifsc-response.ts
@@ -0,0 +1,18 @@
+export interface IFSCResponse {
+  CENTRE:   string;
+  STATE:    string;
+  DISTRICT: string;
+  SWIFT:    string;
+  MICR:     string;
+  BRANCH:   string;
+  CONTACT:  string;
+  NEFT:     boolean;
+  CITY:     string;
+  RTGS:     boolean;
+  UPI:      boolean;
+  IMPS:     boolean;
+  ADDRESS:  string;
+  BANK:     string;
+  BANKCODE: string;
+  IFSC:     string;
+}
